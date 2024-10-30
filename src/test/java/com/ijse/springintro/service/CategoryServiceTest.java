b25fed6aa67448492b5c595236b357ee530021bc
KashmiraNimnada <kashmiranimnada98@gmail.com>
2024-10-30 18:43:40 +0530

    unit testing of getAllCategories method in CategoryService.java

--- src/test/java/com/ijse/springintro/service/CategoryServiceTest.java
+++ src/test/java/com/ijse/springintro/service/CategoryServiceTest.java
@@ -9,8 +9,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Optional;
+import java.util.Arrays;
+import java.util.List;
 
 import com.ijse.springintro.entity.Category;
 import com.ijse.springintro.repository.CategoryRepository;
@@ -48,5 +51,26 @@ public class CategoryServiceTest {
         Assertions.assertTrue(foundCategory.getName()=="Clothes");
     }
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+   
+    @Test
+    void getAllCategoriesShouldGetAllCategories() {
+        
+        Category category1 = Category.builder().id(1L).name("Clothes").build();
+        Category category2 = Category.builder().id(2L).name("Food").build();
+
+        List<Category> categories = Arrays.asList(category1,category2);
+
+        when(categoryRepository.findAll()).thenReturn(categories);
+
+        List<Category> results = categoryServiceImpl.getAllCategories();
+
+        Assertions.assertEquals(categories.get(0).getId(), results.get(0).getId());
+        Assertions.assertTrue(categories.get(1).getName()==results.get(1).getName());
+        Assertions.assertEquals(categories, results);
+    }
     
 }
