8492caf33f8f279024145903cbb7ea2cf49988e8
KashmiraNimnada <kashmiranimnada98@gmail.com>
2024-10-30 18:26:05 +0530

    getCategoryById method in CategoryService tested

--- src/test/java/com/ijse/springintro/service/CategoryServiceTest.java
+++ src/test/java/com/ijse/springintro/service/CategoryServiceTest.java
@@ -1,12 +1,16 @@
 package com.ijse.springintro.service;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
 
 import com.ijse.springintro.entity.Category;
 import com.ijse.springintro.repository.CategoryRepository;
@@ -32,6 +36,17 @@ public class CategoryServiceTest {
         Assertions.assertTrue(createdCategory.getName()=="Clothes");
     }
     
+    @Test
+    void getCategoryByIdShouldGetCategoryById() {
+
+        Category category = Category.builder().id(1L).name("Clothes").build();
+        when(categoryRepository.findById(1L)).thenReturn(Optional.of(category));
+
+        Category foundCategory = categoryServiceImpl.getCategoryById(1L);
+
+        Assertions.assertEquals(category.getId(), foundCategory.getId());
+        Assertions.assertTrue(foundCategory.getName()=="Clothes");
+    }
 
     
 }
